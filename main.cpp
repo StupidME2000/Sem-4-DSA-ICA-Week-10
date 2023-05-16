17a22d77f24060986d71d908bd0c2ea34b7e8c1f
StupidME2000 <thuthinidhijpic.20@uom.lk>
2023-05-16 19:06:36 +0530

    innitial

--- main.cpp
+++ main.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
-#include <vector>
+#include <list>
 using namespace std;
 
+// Structure representing a node in the graph
 struct Node {
-    int label;
-    vector<int> neighbours;
+    int label;                // Node label
+    list<int> neighbours;     // List of neighbouring nodes
 };
 
+// Graph data structure
 struct Graph {
-    int n;
-    Node* nodes;
+    int n = 8;                // Total number of nodes in the graph
+    Node* nodes = new Node[n];  // Array of nodes
 
-    Graph(int numNodes) {
-        n = numNodes;
-        nodes = new Node[n];
-    }
-
+    // Initialize the nodes with labels
     void initializeNodes() {
         for (int i = 0; i < n; i++) {
             nodes[i].label = i + 1;
         }
     }
 
+    // Add an undirected edge between two nodes
     void addEdge(int u, int v) {
+        // Check if the edge (u, v) or (v, u) is already present
+        for (int neighbour : nodes[u - 1].neighbours) {
+            if (neighbour == v) {
+                return;  // Edge already exists
+            }
+        }
+        // If the edge is not already present, add it
         nodes[u - 1].neighbours.push_back(v);
         nodes[v - 1].neighbours.push_back(u);
     }
 
+    // Print the adjacency list of the graph
     void print() {
         for (int i = 0; i < n; i++) {
             cout << "Node " << nodes[i].label << " neighbours: ";
             for (int neighbour : nodes[i].neighbours) {
-                cout << neighbour << " ";
+                cout << "--> " << neighbour;
             }
             cout << endl;
         }
@@ -39,9 +46,10 @@ struct Graph {
 };
 
 int main() {
-    int numNodes = 8;
-    Graph* g = new Graph(numNodes);
+    Graph* g = new Graph;
     g->initializeNodes();
+
+    // Add edges for the graph (undirected)
     g->addEdge(1, 2);
     g->addEdge(1, 3);
     g->addEdge(1, 4);
@@ -49,10 +57,13 @@ int main() {
     g->addEdge(2, 3);
     g->addEdge(2, 6);
     g->addEdge(4, 6);
-    g->addEdge(4, 8);
-    g->addEdge(5, 8);
-    g->addEdge(5, 7);
     g->addEdge(4, 7);
+    g->addEdge(4, 8);
+    g->addEdge(5, 6);
+    g->addEdge(5, 7);
+    g->addEdge(5, 8);
+
+    // Print the graph adjacency list
     g->print();
 
     delete g;
